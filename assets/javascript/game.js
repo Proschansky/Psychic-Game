750c2162de62bdb10eac6546813d18b8f13774c3
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2018-12-04 18:51:23 -0500

    I staged all changes.

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split('').join(',')
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var tries = [];
+
+function inArray(any,array){
+    for(var i=0;i<array.length;i++){
+        if(array[i]==any){return ("true")};
+    };
+};
+
+
+document.getElementById("wins").innerHTML = ("Wins: "+ wins);
+document.getElementById("losses").innerHTML = ("Losses: "+losses);
+document.getElementById("guessesLeft").innerHTML = ("Guesses remaining:" + guessesLeft);
+
+document.onkeypress = function computerGo(event){
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    var userGuess = event.key;
+    
+    if (inArray(userGuess,tries) == "true"){
+        alert("You guessed that one already!");
+        tries.length == tries.remove(userGuess);
+    }
+
+    else if(userGuess === computerChoice){
+        wins++; computerGo;tries=[];
+    }
+
+    else if (guessesLeft === 1){
+        losses++;guessesLeft = (guessesLeft + 9);computerGo;tries=[];
+    }
+    
+    else  guessesLeft--; tries.push(userGuess);
+    
+    document.getElementById('wins').innerHTML = ("Wins: " + wins);
+    document.getElementById('losses').innerHTML = ("Losses: " + losses);
+    document.getElementById('guessesLeft').innerHTML = ("Guesses left: "+ guessesLeft);
+    document.getElementById('tries').innerHTML = ("Your guesses thus far: "+ tries);
+};
+
+
+
+
+
+
+
+
