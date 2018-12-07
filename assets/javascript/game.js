a2ba12fc74f69db3a3fa64966281f4c9dbbc0990
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2018-12-06 19:06:22 -0500

    Debugged

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var tries = [];
+var backgroundColors = ["9ACD32","E8980C","FF0000","4F0CE8","0DE6FF"]
 
 function inArray(any,array){
     for(var i=0;i<array.length;i++){
@@ -11,34 +12,33 @@ function inArray(any,array){
     };
 };
 
-
 document.getElementById("wins").innerHTML = ("Wins: "+ wins);
 document.getElementById("losses").innerHTML = ("Losses: "+losses);
-document.getElementById("guessesLeft").innerHTML = ("Guesses remaining:" + guessesLeft);
+document.getElementById("guessesLeft").innerHTML = ("Guesses remaining: " + guessesLeft);
 
 document.onkeypress = function computerGo(event){
     var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     var userGuess = event.key;
     
-    if (inArray(userGuess,tries) == "true"){
-        alert("You guessed that one already!");
-        tries.length == tries.remove(userGuess);
+    if(userGuess === computerChoice){
+        wins++; computerGo;tries=[];
     }
 
-    else if(userGuess === computerChoice){
-        wins++; computerGo;tries=[];
+    else if (inArray(userGuess,tries)){
+        alert("You guessed that one already!");
+        tries = tries.remove(userGuess);
     }
 
     else if (guessesLeft === 1){
         losses++;guessesLeft = (guessesLeft + 9);computerGo;tries=[];
     }
     
-    else  guessesLeft--; tries.push(userGuess);
+    else guessesLeft--; tries.push(userGuess);
     
     document.getElementById('wins').innerHTML = ("Wins: " + wins);
     document.getElementById('losses').innerHTML = ("Losses: " + losses);
     document.getElementById('guessesLeft').innerHTML = ("Guesses left: "+ guessesLeft);
-    document.getElementById('tries').innerHTML = ("Your guesses thus far: "+ tries);
+    document.getElementById('tries').innerHTML = ("Your guesses thus far:" + tries).toUpperCase();
 };
 
 
